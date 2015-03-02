cd1c08b6e6fe13dfc277b7389127ae54b695ebc9
John Mayan <quemedico@gmail.com>
2015-03-02 16:12:58 -0300

    Creación de aplicaciones

--- ABM/models.py
+++ ABM/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+from django.core.exceptions import ValidationError
+from django.core.validators import RegexValidator
+
+
 # Create your models here.
+class Persona(models.Model):
+    nombre_text = models.CharField(max_length=200)
+    apellido_text = models.CharField(max_length=200)
+    fecha_nacimiento_date = models.DateTimeField('fecha de nacimiento')
+    direccion_text = models.CharField(max_length=200)
+    telefono_text = models.CharField(max_length=200,
+                                     validators=[RegexValidator(regex='^\d{10}$', message='Length has to be 10', code='Invalid number')])
+    dni_number = models.IntegerField(max_length=8)
+
+    class Meta:
+        abstract = True
+
+class Medico(Persona):
+    matricula_text = models.CharField(max_length=200)
+    especialidad_text = models.CharField(max_length=200)
+
+
+class Paciente(Persona):
+    identificador_number = models.IntegerField(primary_key=True)
